736c76246fa0a73298025816ab1fa7ca5afc5ace
Mert Kaplan <mmertkaplan01@hotmail.com>
2025-12-01 23:38:57 +0300

    URL yapılandırmaları düzeltildi

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('authorization.urls')),
+    path('api/auth/', include('authorization.urls')),
 ]
