cd1eb6f2d4d05c3cc3d7c06c7b58a806d8006167
Mert Kaplan <mmertkaplan01@hotmail.com>
2025-12-14 11:26:48 +0300

    - Üye ve fitness sınıfı ilişkilerini içeren rezervasyon modeli eklendi - Unique restriction ile Double booking önlendi - Sınıf kapasitesi doğrulaması uygulandı - Rezervasyon sırasında fiyat hesaplandı ve kaydedildi - Rezervasyon sonrasında fiyatın sabit kalması sağlandı

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Reservation
+from .serializers import ReservationCreateSerializer
+from django.core.exceptions import ValidationError
 
-# Create your views here.
+class ReservationListCreateView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ReservationCreateSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(
+            data=request.data,
+            context={'request': request}
+        )
+
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            reservation = serializer.save()
+        except ValidationError as e:
+            return Response(
+                {"detail": str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return Response(
+            {"id": reservation.id},
+            status=status.HTTP_201_CREATED
+        )
+
+
+class ReservationDetailView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Reservation.objects.all()
