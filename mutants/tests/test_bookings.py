c14c5d4923654bae8dd0f70e62e6d4180240816e
akinmengee <akinmenge8552@gmail.com>
2025-12-22 23:36:54 +0300

    abi bunları kullanmıyosan atıyom

--- mutants/tests/test_bookings.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from django.test import TestCase
-
-class BookingsTestCase(TestCase):
-    """Test cases for bookings functionality"""
-    
-    def test_placeholder(self):
-        """Placeholder test for bookings app"""
-        assert True
--- tests/test_classes.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from django.test import TestCase
-
-class ClassesTestCase(TestCase):
-    """Test cases for fitness classes functionality"""
-    
-    def test_placeholder(self):
-        """Placeholder test for classes app"""
-        assert True
